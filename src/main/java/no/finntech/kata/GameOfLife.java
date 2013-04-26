2578152d42234d6ab3ee51b9f6bc02d2d2adfcc9
Tommy Johansen <tommy.johansen@finn.no>
2013-04-26 13:25:32 +0200

    Overcrowd

--- src/main/java/no/finntech/kata/GameOfLife.java
+++ src/main/java/no/finntech/kata/GameOfLife.java
@@ -18,7 +18,48 @@ public final class GameOfLife {
         cells[x][y] = true;
     }
 
+    public void setDead(int x, int y) {
+        cells[x][y] = false;
+    }
+
     public boolean isAlive(int x, int y) {
         return cells[x][y];
+    }
+
+    public void evolve() {
+        boolean[][] evolvedCells = new boolean[cells.length][cells[0].length];
+        for(int i = 0; i < cells.length; i++) {
+            for(int j = 0; j < cells[i].length; j++) {
+                int liveCount = countLiveNeighbours(i, j);
+                underpopulationRule(evolvedCells, i, j, liveCount);
+            }
+        }
+        cells = evolvedCells;
+    }
+
+    private void underpopulationRule(boolean[][] evolvedCells, int i, int j, long liveCount) {
+        if(liveCount < 2) {
+            evolvedCells[i][j] = false;
+        } else {
+            evolvedCells[i][j] = true;
+        }
+    }
+
+    private int countLiveNeighbours(int x, int y) {
+        int liveCount = 0;
+        for(int i = x - 1; i < x + 1; i++) {
+            if(i < 0 || i > cells.length - 1) {
+                continue;
+            }
+            for(int j = y - 1; j < y +1; j++) {
+                if(j < 0 || j > cells[0].length - 1) {
+                    continue;
+                }
+                if(isAlive(i, j)) {
+                    liveCount++;
+                }
+            }
+        }
+        return liveCount;
     }
 }
--- src/test/java/no/finntech/kata/GameOfLifeTest.java
+++ src/test/java/no/finntech/kata/GameOfLifeTest.java
@@ -28,8 +28,27 @@ public final class GameOfLifeTest {
     }
 
     @Test
-    public void testUnderpopulation() {
+    public void testUnderpopulation_withoutNeighbours() {
         game.setAlive(1,1);
+        game.evolve();
+        assertThat(game.isAlive(1,1)).isFalse();
+    }
+
+    @Test
+    public void testUnderpopulation_withTwoNeighbours() {
+        game.setAlive(1,1);
+        game.setAlive(0,1);
+        game.setAlive(2,1);
+        game.evolve();
+        assertThat(game.isAlive(1,1)).isTrue();
+    }
+
+    @Test
+    public void testOvercrowding_withTwoNeighbours() {
+        game.setAlive(1,1);
+        game.setAlive(0,1);
+        game.setAlive(2,1);
+        game.setAlive(2,2);
         game.evolve();
         assertThat(game.isAlive(1,1)).isFalse();
     }
